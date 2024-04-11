f21512fe9f4d159524c4237c3a61af1ff57928c0
nghaiyen <hiing.haiyenn@gmail.com>
2024-04-11 20:41:05 +0700

    message

--- detail.js
+++ detail.js
@@ -16,3 +16,4 @@ $('.product__img-sub').slick({
   prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
     nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
 });
+//
